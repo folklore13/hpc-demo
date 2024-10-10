b1e10a1be6b4d1060583ba55128230b662ce189c
Ario Mochammad Azizhi <ario.hitmeup@gmail.com>
2024-10-10 20:09:02 +0700

    Enables users to edit their profile data

--- resources/views/dashboard/home.blade.php
+++ resources/views/dashboard/home.blade.php
@@ -38,19 +38,21 @@
             </div>
         </div>
 
-        <div class="col-xl-3 col-md-6 mb-4">
-            <div class="card border-left-warning shadow h-100 py-2">
-                <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-                                Jumlah User</div>
-                            <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $user }}</div>
+        @can('manage-data')
+            <div class="col-xl-3 col-md-6 mb-4">
+                <div class="card border-left-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                    Jumlah User</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $user }}</div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        @endcan
 
     </div>
 
